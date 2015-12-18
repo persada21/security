259806d3c4515ac9bee3709c188517bfc55c61de
M.Persada <ersad40@gmail.com>
2015-12-18 03:18:07 -0500

    first commit

---
+++ app/mailers/confirmation_mailer.rb
@@ -0,0 +1,17 @@
+class ConfirmationMailer < ActionMailer::Base
+  def confirm_email(target_email, activation_token)
+
+            @activation_token = activation_token
+
+            mail(:to => target_email,
+
+                        :from => "develror@gmail.com",
+
+                        :subject => "[Training - Rails 4]") do |format|
+
+                            format.html { render 'confirm_email'}
+
+                        end
+
+        end
+end
---
+++ db/migrate/20151216034739_add_salt_hash_to_users.rb
@@ -0,0 +1,9 @@
+class AddSaltHashToUsers < ActiveRecord::Migration
+  def change
+
+            add_column :users, :password_salt, :string
+
+            rename_column :users, :password, :password_hash
+
+        end
+end
